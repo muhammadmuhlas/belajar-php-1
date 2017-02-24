c3024c470ae6943f5b2b3b3b0f18d04ec4b73d76
rizasatyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-02-24 20:27:54 +0700

    new Hendrity

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+//hahahha
+
+$nama = "Riza";
+$umur = 19;
+$ipk = 4.0;
+
+echo "Halo, nama ku " . $nama . " Umur temen ku " . $umur . " ipk dosen ku " . $ipk;
+ 
+echo "\n";
+
+if ($ipk == 3.96){
+echo "Hendrity Sejati";
+}else{
+echo "Bukan Hendrity"
+
+}
+
+
+
+
+?>
